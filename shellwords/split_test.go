4199f4f12b4289bfc2dccd5a5cf27af166e1ece0
Simon Menke <simon.menke@gmail.com>
2013-06-03 17:10:21 +0200

    First import

---
+++ shellwords/split_test.go
@@ -0,0 +1,76 @@
+package shellwords
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestSimpleWord(t *testing.T) {
+	var (
+		words []string
+		err   error
+	)
+
+	words, err = Split(`a-word`)
+	equal(t, err, []string{"a-word"}, words)
+
+	words, err = Split(`a-word foo bar`)
+	equal(t, err, []string{"a-word", "foo", "bar"}, words)
+}
+
+func TestSingleQuoteWord(t *testing.T) {
+	var (
+		words []string
+		err   error
+	)
+
+	words, err = Split(`'a-word'`)
+	equal(t, err, []string{"a-word"}, words)
+
+	words, err = Split(`'a-word' 'foo' 'bar'`)
+	equal(t, err, []string{"a-word", "foo", "bar"}, words)
+
+	words, err = Split(`'a-word' 'foo' cool'bar'`)
+	equal(t, err, []string{"a-word", "foo", "coolbar"}, words)
+}
+
+func TestDoubleQuoteWord(t *testing.T) {
+	var (
+		words []string
+		err   error
+	)
+
+	words, err = Split(`"a-word"`)
+	equal(t, err, []string{"a-word"}, words)
+
+	words, err = Split(`"a-word" "foo" "bar"`)
+	equal(t, err, []string{"a-word", "foo", "bar"}, words)
+
+	words, err = Split(`"a \"word\"" "foo \\ baz" cool"bar"`)
+	equal(t, err, []string{"a \"word\"", "foo \\ baz", "coolbar"}, words)
+}
+
+func equal(t *testing.T, e error, exp, act []string) {
+	if e != nil {
+		t.Fatalf("err: %s", e)
+	}
+
+	err := false
+
+	if len(exp) != len(act) {
+		err = true
+	} else {
+		for i, w := range exp {
+			if w != act[i] {
+				err = true
+				break
+			}
+		}
+	}
+
+	if err {
+		exp_json, _ := json.Marshal(exp)
+		act_json, _ := json.Marshal(act)
+		t.Fatalf("expected %s but got %s", exp_json, act_json)
+	}
+}
